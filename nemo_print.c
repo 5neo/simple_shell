54607f91742ec5bd5f93f88917c1f75cd4110281
Walker <abdalqadermomen@gmail.com>
2023-12-14 02:42:58 +0200

    nemo print

---
+++ nemo_print.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/*
+ * nemo_print - Prints the provided message to the standard output.
+ *
+ * @message: A pointer to the null-terminated string to be printed.
+ *
+ * Return: This function returns nothing.
+ */
+void nemo_print(const char *message) {
+    write(STDOUT_FILENO, message, strlen(message));
+}
