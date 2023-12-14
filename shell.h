fb1740f0367ab766c9858c6d648746ee05f37f18
Walker <abdalqadermomen@gmail.com>
2023-12-14 02:44:00 +0200

    shell header file

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+/*
+ * Header file for a simple shell program.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+
+
+void display_prompt(void);
+void nemo_print(const char *message);
+void readCommand(char *command, size_t size);
+void execCommand(const char *command);
+
+
+
+
+#endif /* SHELL_H */
